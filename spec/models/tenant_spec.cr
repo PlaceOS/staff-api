5408a61e62046959f942c3b74fccad4f7fc6911a
Toby Carvan <toby.carvan@gmail.com>
2020-09-25 13:58:19 +1000

    support json data for Tenants#create

--- spec/models/tenant_spec.cr
+++ spec/models/tenant_spec.cr
@@ -12,6 +12,37 @@ describe Tenant do
     a.errors.size.should eq 0
   end
 
+  it "should accept JSON params" do
+    response = IO::Memory.new
+    body = IO::Memory.new
+    body << %({
+      "name":        "Bob",
+      "platform":    "google",
+      "domain":      "club-bob.staff-api.dev",
+      "credentials": {
+        "issuer":      "1122121212",
+        "scopes":      ["http://example.com"],
+        "signing_key": "-----BEGIN PRIVATE KEY-----SOMEKEY DATA-----END PRIVATE KEY-----",
+        "domain":      "example.com.au",
+        "sub":         "bob@example.com.au"
+      }
+    })
+    body.rewind
+
+    headers = HTTP::Headers{
+      "Host"          => "google.staff-api.dev",
+      "Authorization" => "Bearer #{google_mock_token}",
+      "Content-Type"  => "application/json"
+    }
+
+    context = context("POST", "/api/staff/v1/tenants", headers, body, response_io: response)
+
+    Tenants.new(context).create
+    created_tenant = extract_json(response)
+
+    extract_http_status(response).should eq("200")
+  end
+
   it "takes JSON credentials and returns a PlaceCalendar::Client" do
     a = Tenant.query.find! { domain == "toby.staff-api.dev" }
     a.place_calendar_client.class.should eq(PlaceCalendar::Client)
