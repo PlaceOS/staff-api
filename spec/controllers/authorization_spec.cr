8510ac3ceabc9d66bf5836f227827776c0df7a4e
Anh (Duke) Nguyen <58082199+dukeraphaelng@users.noreply.github.com>
2021-01-18 16:02:44 +1100

    chore: authorization spec context spec refactor

--- spec/controllers/authorization_spec.cr
+++ spec/controllers/authorization_spec.cr
@@ -1,31 +1,23 @@
 require "../spec_helper"
 
 describe "Authorization" do
-  it "raise Error::Unauthorized (status: 403) if the domain in the header doesn't match the token" do
-    headers = HTTP::Headers{
-      "Host"          => "wrong.staff-api.dev",
-      "Authorization" => "Bearer #{office_mock_token}",
-    }
-
-    calendars = Calendars.new(context("GET", "/api/staff/v1/calendars", headers))
-
-    # Test the instance method of the controller
-    expect_raises(Error::Unauthorized) do
-      calendars.index
+  describe "raise Error::Unauthorized (status: 403)" do
+    it "when the domain in the header doesn't match the token" do
+      expect_raises(Error::Unauthorized) do
+        Calendars.context("GET", "/api/staff/v1/calendars", {
+          "Host"          => "wrong.staff-api.dev",
+          "Authorization" => "Bearer #{office_mock_token}",
+        }, &.index)
+      end
     end
-  end
 
-  it "raise Error::Unauthorized (status: 403) if the token is invalid" do
-    headers = HTTP::Headers{
-      "Host"          => "toby.staff-api.dev",
-      "Authorization" => "Bearer #{office_mock_token}e",
-    }
-
-    calendars = Calendars.new(context("GET", "/api/staff/v1/calendars", headers))
-
-    # Test the instance method of the controller
-    expect_raises(Error::Unauthorized) do
-      calendars.index
+    it "when the token is invalid" do
+      expect_raises(Error::Unauthorized) do
+        Calendars.context("GET", "/api/staff/v1/calendars", {
+          "Host"          => "toby.staff-api.dev",
+          "Authorization" => "Bearer #{office_mock_token}e",
+        }, &.index)
+      end
     end
   end
 end
