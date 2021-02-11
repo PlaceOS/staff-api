368cf2ffd23399f3cc46b7c3e0299c22d4415e3d
Stephen von Takach <steve@vontaka.ch>
2021-02-11 14:12:52 +1100

    feat: add database connection check to health check
    so we can auto-recover the service

--- src/controllers/health_check.cr
+++ src/controllers/health_check.cr
@@ -2,6 +2,7 @@ class HealthCheck < ActionController::Base
   base "/api/staff/v1"
 
   def index
+    Clear::Migration::Manager.instance.load_existing_migrations
     render json: {
       commit:     App::BUILD_COMMIT,
       build_time: App::BUILD_TIME,
