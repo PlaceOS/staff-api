8964016cdceda62bc8e6b341757b1c432b79f74b
Stephen von Takach <steve@vontaka.ch>
2021-02-24 16:46:57 +1100

    fix booking IDs again

--- src/staff-api.cr
+++ src/staff-api.cr
@@ -39,13 +39,13 @@ OptionParser.parse(ARGV.dup) do |parser|
     failed = 0
     clash_check = [] of Tuple(String, Int64, String)
     Booking.query.by_zones(["zone-G5o0Bdt~Y78"]).each do |booking|
-      if booking.asset_id.starts_with? "area-F"
+      if booking.asset_id.starts_with? "desk-KG"
+        next if booking.asset_id.starts_with? "desk-KG0"
+
         # ID looks like: area-F.16.30-status
         puts "updating booking #{booking.id} for #{booking.asset_id}"
-        parts = booking.asset_id.split('-')[1].split('.')
-        level_id = parts[1]
-        desk_id = parts[2]
-        booking.asset_id = "desk-KG#{level_id}.#{desk_id.rjust(3,'0')}F"
+        booking.asset_id = booking.asset_id.gsub("desk-KG", "desk-KG0")
+        booking.description = booking.description.split('-')[0] + "-#{booking.asset_id}"
         if booking.save
           success += 1
           clash_check << {booking.user_email, booking.booking_start, booking.asset_id}
