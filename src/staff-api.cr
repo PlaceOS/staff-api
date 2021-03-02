4c710010b0d34ca5cb8f4fd483728c05cd4f2e82
Stephen von Takach <steve@vontaka.ch>
2021-03-02 13:18:38 +1100

    add basic email migration

--- src/staff-api.cr
+++ src/staff-api.cr
@@ -34,54 +34,30 @@ OptionParser.parse(ARGV.dup) do |parser|
     exit 0
   end
 
-  parser.on("-f", "--fix", "Fixes booking asset IDs as a once off") do
+  parser.on("-f", "--fix", "Fixes booking owner emails") do
     success = 0
     failed = 0
-    fixed_description = 0
-    clash_check = [] of Tuple(String, Int64, String, Int64)
-    Booking.query.by_zones(["zone-G5o6CPdNWUc"]).each do |booking|
-      if booking.asset_id.starts_with? "area-F"
-        # ID looks like: area-F.16.30-status
-        puts "updating booking #{booking.id} for #{booking.asset_id}"
-        parts = booking.asset_id.split('-')[1].split('.')
-        level_id = parts[1]
-        desk_id = parts[2]
-        booking.asset_id = "desk-BR#{level_id.rjust(2,'0')}.#{desk_id.rjust(3,'0')}F"
-        if description = booking.description
-          booking.description = description.split("-")[0] + "-#{booking.asset_id}"
-        end
-        if booking.save
-          success += 1
-          clash_check << {booking.user_email, booking.booking_start, booking.asset_id, booking.booking_end}
-        else
-          failed += 1
-        end
-      elsif (description = booking.description) && description.includes?("-area-F")
-        booking.description = description.split("-")[0] + "-#{booking.asset_id}"
-        if booking.save
-          fixed_description += 1
-          success += 1
-          clash_check << {booking.user_email, booking.booking_start, booking.asset_id, booking.booking_end}
-        else
-          failed += 1
-        end
+    Booking.query.each do |booking|
+      if booking.save
+        success += 1
+      else
+        failed += 1
       end
     end
-    puts "found #{success + failed + fixed_description} successfully updated #{success} bookings, fixed description on #{fixed_description} bookings"
+    puts "checked #{success + failed}, successfully updated #{success} bookings"
 
-    puts "checking for clashes..."
-    puts "asset_id, user_email, booking_start, booking_end"
-    clashes = 0
-    clash_check.each do |(user_email, booking_start, asset_id, booking_end)|
-      if Booking.query.where(
-          "booking_start = :starting AND asset_id = :asset",
-          starting: booking_start, asset: asset_id
-        ).count > 1_i64
-        clashes += 1
-        puts "#{asset_id}, #{user_email}, #{booking_start}, #{booking_end}"
+    success = 0
+    failed = 0
+
+    Guest.query.each do |guest|
+      if guest.save
+        success += 1
+      else
+        failed += 1
       end
     end
-    puts "found #{clashes} clashes"
+
+    puts "checked #{success + failed}, successfully updated #{success} guests"
 
     exit 0
   end
