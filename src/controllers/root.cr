91ee6ecc1b3d603ac2617503962b0dbdfc7c3cd3
tassja <xtassja@gmail.com>
2021-06-24 16:08:14 +1000

    fix: root base

--- src/controllers/root.cr
+++ src/controllers/root.cr
@@ -2,9 +2,9 @@ require "placeos-models/version"
 
 module App
   class Root < Application
-    base "/api/staff/v1/version"
+    base "/api/staff/v1"
 
-    get "/", :version do
+    get "/version", :version do
       render :ok, json: PlaceOS::Model::Version.new(
         version: App::VERSION,
         build_time: App::BUILD_TIME,
