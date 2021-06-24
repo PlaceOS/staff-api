4efe0a92d510280f0d6df328d7cfd329179cb3fd
tassja <xtassja@gmail.com>
2021-06-24 15:53:57 +1000

    fix:  incorrect service name in test

--- spec/controllers/root_spec.cr
+++ spec/controllers/root_spec.cr
@@ -1,14 +1,9 @@
 require "../spec_helper"
 
 with_server do
-  # it "health checks" do
-  #   result = curl("GET", "/api/frontends/v1/")
-  #   result.success?.should be_true
-  # end
-
   it "should check version" do
-    result = curl("GET", "/api/frontends/v1/version")
+    result = curl("GET", "/api/staff/v1/version")
     result.status_code.should eq 200
-    PlaceOS::Model::Version.from_json(result.body).service.should eq "frontends"
+    PlaceOS::Model::Version.from_json(result.body).service.should eq "StaffAPI"
   end
 end
