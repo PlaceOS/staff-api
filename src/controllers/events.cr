d8bc41a1b5cd9cd8e6bbcd71e897ccd079f97837
Stephen von Takach <steve@place.tech>
2021-09-09 15:38:04 +1000

    feat(events): add permissions check on event creation (#111)

--- src/controllers/events.cr
+++ src/controllers/events.cr
@@ -93,7 +93,10 @@ class Events < Application
     input_event = PlaceCalendar::Event.from_json(request.body.as(IO))
     placeos_client = get_placeos_client
 
-    host = input_event.host || user.email
+    # get_user_calendars returns only calendars where the user has write access
+    user_email = user.email.downcase
+    host = input_event.host.try(&.downcase) || user_email
+    head :forbidden unless host == user_email || get_user_calendars.find { |cal| cal.id.try(&.downcase) == host }
 
     system_id = input_event.system_id || input_event.system.try(&.id)
     if system_id
