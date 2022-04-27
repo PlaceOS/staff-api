614dfebae9fbe7639287939e279208a0c20b6b3b
Stephen von Takach <steve@place.technology>
2022-04-27 10:40:36 +1000

    change how we append things

--- src/migrations/0017_migrate_user_id.cr
+++ src/migrations/0017_migrate_user_id.cr
@@ -4,8 +4,8 @@ class MigrateUserID
   def change(direction)
     direction.up do
       # ensure all user_id fields are prefixed
-      execute("UPDATE bookings SET user_id = 'user-' + user_id WHERE user_id NOT LIKE 'user-%'")
-      execute("UPDATE bookings SET approver_id = 'user-' + user_id WHERE approver_id IS NOT NULL AND approver_id NOT LIKE 'user-%'")
+      execute("UPDATE bookings SET user_id = N'user-' + user_id WHERE user_id NOT LIKE 'user-%'")
+      execute("UPDATE bookings SET approver_id = N'user-' + user_id WHERE approver_id IS NOT NULL AND approver_id NOT LIKE 'user-%'")
     end
   end
 end
