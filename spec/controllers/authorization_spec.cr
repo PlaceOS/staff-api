bd9160a04c17424f202ffc0ffc198e071ecd9e68
Stephen von Takach <steve@place.technology>
2022-06-08 00:31:03 +1000

    fix format

--- spec/controllers/authorization_spec.cr
+++ spec/controllers/authorization_spec.cr
@@ -4,7 +4,7 @@ describe "Authorization" do
   describe "raise Error::Unauthorized (status: 403)" do
     it "when the domain in the header doesn't match the token" do
       expect_raises(Error::Unauthorized) do
-        Calendars.spec_instance(HTTP::Request.new("GET",  Calendars.base_route, headers: HTTP::Headers{
+        Calendars.spec_instance(HTTP::Request.new("GET", Calendars.base_route, headers: HTTP::Headers{
           "Host"          => "wrong.staff-api.dev",
           "Authorization" => "Bearer #{Mock::Token.office}",
         })).index
@@ -13,7 +13,7 @@ describe "Authorization" do
 
     it "when the token is invalid" do
       expect_raises(Error::Unauthorized) do
-        Calendars.spec_instance(HTTP::Request.new("GET",  Calendars.base_route, headers: HTTP::Headers{
+        Calendars.spec_instance(HTTP::Request.new("GET", Calendars.base_route, headers: HTTP::Headers{
           "Host"          => "toby.staff-api.dev",
           "Authorization" => "Bearer #{Mock::Token.office}e",
         })).index
