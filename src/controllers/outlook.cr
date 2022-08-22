49adb425f472745fa0b9c5d312e065c760d872a6
Mia Bennett <mia@chillfox.com>
2022-08-22 11:11:47 +0930

    feat(outlook): remove copied endpoint

--- src/controllers/outlook.cr
+++ src/controllers/outlook.cr
@@ -1,14 +1,6 @@
 class Outlook < ActionController::Base
   base "/api/staff/v1/outlook"
 
-  def index
-    Clear::Migration::Manager.instance.load_existing_migrations
-    render json: {
-      commit:     App::BUILD_COMMIT,
-      build_time: App::BUILD_TIME,
-    }
-  end
-
   get("/manifest.xml") do
     # TODO: get values from config
     manifest = OutlookManifest.new(
