e6163af42dd36c344d09d111b7e16543d1628566
Mia Bennett <mia@chillfox.com>
2022-11-15 11:13:45 +0930

    feat(survey): add survey endponts

---
+++ src/controllers/surveys.cr
@@ -0,0 +1,62 @@
+class Surveys < Application
+  base "/api/staff/v1/surveys"
+
+  # =====================
+  # Filters
+  # =====================
+
+  @[AC::Route::Filter(:before_action, except: [:index, :create])]
+  private def find_survey(id : Int64)
+    @survey = Survey.find!(id)
+  end
+
+  getter! survey : Survey
+
+  # =====================
+  # Routes
+  # =====================
+
+  # returns a list of surveys
+  @[AC::Route::GET("/")]
+  def index : Array(Survey::Responder)
+    Survey.query.select("id, title, description, question_order").to_a.map(&.as_json)
+  end
+
+  # creates a new survey
+  @[AC::Route::POST("/", body: :survey_body, status_code: HTTP::Status::CREATED)]
+  def create(survey_body : Survey::Responder) : Survey::Responder
+    survey = survey_body.to_survey
+    raise Error::ModelValidation.new(survey.errors.map { |error| {field: error.column, reason: error.reason} }, "error validating survey data") if !survey.create
+    survey.as_json
+  end
+
+  # patches an existing survey
+  @[AC::Route::PUT("/:id", body: :survey_body)]
+  @[AC::Route::PATCH("/:id", body: :survey_body)]
+  def update(survey_body : Survey::Responder) : Survey::Responder
+    changes = survey_body.to_survey(update: true)
+
+    {% for key in [:title, :description, :question_order] %}
+      begin
+        survey.{{key.id}} = changes.{{key.id}} if changes.{{key.id}}_column.defined?
+      rescue NilAssertionError
+      end
+    {% end %}
+
+    raise Error::ModelValidation.new(survey.errors.map { |error| {field: error.column, reason: error.reason} }, "error validating survey data") if !survey.save
+    survey.as_json
+  end
+
+  def show(
+    @[AC::Param::Info(name: "id", description: "the survey id", example: "...")]
+    survey_id : String
+  ) : Survey::Responder
+    survey.as_json
+  end
+
+  # deletes the survey
+  @[AC::Route::DELETE("/:id", status_code: HTTP::Status::ACCEPTED)]
+  def destroy : Nil
+    survey.delete
+  end
+end
--- src/models/survey.cr
+++ src/models/survey.cr
@@ -9,8 +9,60 @@ class Survey
   # has_many questions : Survey::Question, foreign_key: "survey_id"
   has_many answers : Survey::Answer, foreign_key: "answer_id"
 
-  # before save
-  # check that question_order is unique (no duplicate questions)
-
   timestamps
+
+  struct Responder
+    include JSON::Serializable
+
+    getter id : Int64?
+    getter title : String?
+    getter description : String?
+    getter question_order : Array(Int64)?
+
+    def initialize(@id, @title = nil, @description = nil, @question_order = nil)
+    end
+
+    def to_survey(update : Bool = false)
+      survey = Survey.new
+      {% for key in [:title, :description] %}
+        survey.{{key.id}} = self.{{key.id}}.not_nil! unless self.{{key.id}}.nil?
+      {% end %}
+
+      if q_order = question_order
+        survey.question_order = q_order unless update && q_order.as_a.empty?
+      elsif !update
+        survey.question_order = [] of Int64
+      end
+
+      survey
+    end
+  end
+
+  def as_json
+    description = description_column.defined? ? self.description : ""
+    question_order = question_order_column.defined? ? self.question_order : [] of Int64
+
+    Responder.new(
+      id: self.id,
+      title: self.title,
+      description: self.description,
+      question_order: self.question_order,
+    )
+  end
+
+  def validate
+    validate_columns
+    validate_question_order
+  end
+
+  private def validate_columns
+    add_error("title", "must be defined") unless title_column.defined?
+    add_error("question_order", "must be defined") unless question_order_column.defined?
+  end
+
+  private def validate_question_order
+    if question_order_column.defined?
+      question_order == question_order.unique
+    end
+  end
 end
