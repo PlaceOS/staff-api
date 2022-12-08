61b3a99957927f0bc63e6d56f3d5b495cc211c10
Mia Bennett <mia@chillfox.com>
2022-12-08 14:59:18 +0930

    feat(surveys): add endpoint for getting answers

--- src/controllers/surveys/answers.cr
+++ src/controllers/surveys/answers.cr
@@ -1,6 +1,18 @@
 class Surveys::Answers < Application
   base "/api/staff/v1/surveys/answers"
 
+  # returns a list of answers
+  @[AC::Route::GET("/")]
+  def index(
+    @[AC::Param::Info(description: "the survey id to get answers for", example: "123")]
+    survey_id : Int64? = nil,
+  ) : Array(Survey::Answer::Responder)
+    query = Survey::Answer.query.select("id, question_id, survey_id, answer_text, answer_json")
+    query.where(survey_id: survey_id) if survey_id
+
+    query.to_a.map(&.as_json)
+  end
+
   # creates a new survey answer
   @[AC::Route::POST("/", body: :answer_body, status_code: HTTP::Status::CREATED)]
   def create(answer_body : Array(Survey::Answer::Responder)) : Array(Survey::Answer::Responder)
