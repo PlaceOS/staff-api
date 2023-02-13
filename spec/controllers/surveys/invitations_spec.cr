d328fc02c9acdf48970183c92f20bfd714357e8d
Mia Bennett <mia@chillfox.com>
2023-02-13 13:04:21 +0930

    test(surveys): comprehensive specs for GET /surveys/invitations

--- spec/controllers/surveys/invitations_spec.cr
+++ spec/controllers/surveys/invitations_spec.cr
@@ -5,6 +5,64 @@ require "../helpers/survey_helper"
 describe Surveys::Invitations do
   client = AC::SpecHelper.client
   headers = Mock::Headers.office365_guest
+
+  describe "#index" do
+    it "should return a list of invitations" do
+      survey = SurveyHelper.create_survey
+      invitations = [
+        SurveyHelper.create_invitation(survey: survey, email: "user1@spec.test", sent: false),
+        SurveyHelper.create_invitation(survey: survey, email: "user2@spec.test", sent: false),
+      ]
+
+      response = client.get(INVITATIONS_BASE, headers: headers)
+      response.status_code.should eq(200)
+      response_json = JSON.parse(response.body)
+      response_json.as_a.map(&.["email"]).should eq(invitations.map(&.email))
+    end
+
+    it "should return a list of invitations for a survey" do
+      survey1 = SurveyHelper.create_survey
+      survey2 = SurveyHelper.create_survey
+      invitations = [
+        SurveyHelper.create_invitation(survey: survey1, email: "user1@spec.test", sent: false),
+        SurveyHelper.create_invitation(survey: survey2, email: "user2@spec.test", sent: false),
+      ]
+
+      response = client.get("#{INVITATIONS_BASE}?survey_id=#{survey1.id}", headers: headers)
+      response.status_code.should eq(200)
+      response_json = JSON.parse(response.body)
+      response_json.as_a.map(&.["id"]).should contain(invitations[0].id)
+      response_json.as_a.map(&.["id"]).should_not contain(invitations[1].id)
+    end
+
+    it "should return a list of sent invitations" do
+      survey = SurveyHelper.create_survey
+      invitations = [
+        SurveyHelper.create_invitation(survey: survey, email: "user1@spec.test", sent: true),
+        SurveyHelper.create_invitation(survey: survey, email: "user2@spec.test", sent: false),
+      ]
+
+      response = client.get("#{INVITATIONS_BASE}?sent=true", headers: headers)
+      response.status_code.should eq(200)
+      response_json = JSON.parse(response.body)
+      response_json.as_a.map(&.["id"]).should contain(invitations[0].id)
+      response_json.as_a.map(&.["id"]).should_not contain(invitations[1].id)
+    end
+
+    it "should return a list of not sent invitations" do
+      survey = SurveyHelper.create_survey
+      invitations = [
+        SurveyHelper.create_invitation(survey: survey, email: "user1@spec.test", sent: true),
+        SurveyHelper.create_invitation(survey: survey, email: "user2@spec.test", sent: false),
+      ]
+
+      response = client.get("#{INVITATIONS_BASE}?sent=false", headers: headers)
+      response.status_code.should eq(200)
+      response_json = JSON.parse(response.body)
+      response_json.as_a.map(&.["id"]).should_not contain(invitations[0].id)
+      response_json.as_a.map(&.["id"]).should contain(invitations[1].id)
+    end
+  end
 end
 
-Invitations_BASE = Surveys::Invitations.base_route
+INVITATIONS_BASE = Surveys::Invitations.base_route
---
+++ spec/controllers/surveys/triggers_spec.cr
@@ -0,0 +1,31 @@
+require "../../spec_helper"
+require "../helpers/spec_clean_up"
+require "../helpers/survey_helper"
+require "../helpers/booking_helper"
+
+describe "Survey Triggers" do
+  client = AC::SpecHelper.client
+  headers = Mock::Headers.office365_guest
+
+  pending "should create an invitation on booking state change" do
+    survey = SurveyHelper.create_survey(
+      zone_id: "zone-3",
+      building_id: "zone-1",
+      trigger: "RESERVED",
+    )
+
+    tenant = Tenant.query.find! { domain == "toby.staff-api.dev" }
+    booking = BookingsHelper.create_booking(
+      tenant_id: tenant.id,
+      user_email: "user@example.com",
+      zones: ["zone-1", "zone-2"],
+    )
+
+    invitations = Survey::Invitation.query.select("id").map(&.id)
+    # pp "########################################"
+    # pp! booking.current_state
+    # pp! booking.history.map(&.state)
+    # pp! invitations
+    # pp "########################################"
+  end
+end
