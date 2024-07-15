79a0f5e061c16984fce0e70b497940cd921b0e18
Mia Bennett <mia@chillfox.com>
2024-07-15 14:24:38 +0930

    test(events): #index

--- spec/controllers/events_spec.cr
+++ spec/controllers/events_spec.cr
@@ -432,9 +432,99 @@ describe Events do
     end
 
     pending "#index should return a list of PUBLIC events for unauthenticated users" do
+      WebMock.stub(:get, "https://graph.microsoft.com/v1.0/users/dev%40acaprojects.com/calendar?")
+        .to_return(body: File.read("./spec/fixtures/calendars/o365/show.json"))
+      WebMock.stub(:get, "#{ENV["PLACE_URI"]}/api/engine/v2/systems?limit=1000&offset=0&zone_id=z1")
+        .to_return(body: File.read("./spec/fixtures/placeos/systems.json"))
+      WebMock.stub(:post, "https://graph.microsoft.com/v1.0/%24batch")
+        .to_return(body: File.read("./spec/fixtures/events/o365/batch_index.json"))
+
+      tenant = get_tenant
+
+      # private booking
+      private_event = EventMetadatasHelper.create_event(
+        tenant.id,
+        event_start: 10.minutes.from_now.to_unix,
+        event_end: 50.minutes.from_now.to_unix,
+        permission: PlaceOS::Model::EventMetadata::Permission::PRIVATE,
+      )
+
+      # open booking
+      open_event = EventMetadatasHelper.create_event(
+        tenant.id,
+        event_start: 10.minutes.from_now.to_unix,
+        event_end: 50.minutes.from_now.to_unix,
+        permission: PlaceOS::Model::EventMetadata::Permission::OPEN,
+      )
+
+      # public booking
+      public_event = EventMetadatasHelper.create_event(
+        tenant.id,
+        event_start: 10.minutes.from_now.to_unix,
+        event_end: 50.minutes.from_now.to_unix,
+        permission: PlaceOS::Model::EventMetadata::Permission::PUBLIC,
+      )
+
+      starting = 5.minutes.from_now.to_unix
+      ending = 90.minutes.from_now.to_unix
+
+      # public user
+      no_auth_headers = Mock::Headers.office365_no_auth
+      response = client.get("#{EVENTS_BASE}?zone_ids=z1&period_start=#{starting}&period_end=#{starting}", headers: no_auth_headers)
+      response.status_code.should eq(200)
+      events = JSON.parse(response.body).as_a
+      events.size.should eq(1)
+      events.map(&.["id"]).should_not contain(private_event.id)
+      events.map(&.["id"]).should_not contain(open_event.id)
+      events.map(&.["id"]).should contain(public_event.id)
     end
 
     pending "#index should return a list of OPEN and PUBLIC events for same tenant users" do
+      WebMock.stub(:get, "https://graph.microsoft.com/v1.0/users/dev%40acaprojects.com/calendar?")
+        .to_return(body: File.read("./spec/fixtures/calendars/o365/show.json"))
+      WebMock.stub(:get, "#{ENV["PLACE_URI"]}/api/engine/v2/systems?limit=1000&offset=0&zone_id=z1")
+        .to_return(body: File.read("./spec/fixtures/placeos/systems.json"))
+      WebMock.stub(:post, "https://graph.microsoft.com/v1.0/%24batch")
+        .to_return(body: File.read("./spec/fixtures/events/o365/batch_index.json"))
+
+      tenant = get_tenant
+
+      # private booking
+      private_event = EventMetadatasHelper.create_event(
+        tenant.id,
+        event_start: 10.minutes.from_now.to_unix,
+        event_end: 50.minutes.from_now.to_unix,
+        permission: PlaceOS::Model::EventMetadata::Permission::PRIVATE,
+      )
+
+      # open booking
+      open_event = EventMetadatasHelper.create_event(
+        tenant.id,
+        event_start: 10.minutes.from_now.to_unix,
+        event_end: 50.minutes.from_now.to_unix,
+        permission: PlaceOS::Model::EventMetadata::Permission::OPEN,
+      )
+
+      # public booking
+      public_event = EventMetadatasHelper.create_event(
+        tenant.id,
+        event_start: 10.minutes.from_now.to_unix,
+        event_end: 50.minutes.from_now.to_unix,
+        permission: PlaceOS::Model::EventMetadata::Permission::PUBLIC,
+      )
+
+      starting = 5.minutes.from_now.to_unix
+      ending = 90.minutes.from_now.to_unix
+
+      # same tenant user
+      same_tenant_headers = Mock::Headers.office365_normal_user(email: "user-four@example.com")
+      response = client.get("#{EVENTS_BASE}?zone_ids=z1&period_start=#{starting}&period_end=#{starting}", headers: same_tenant_headers)
+      response.status_code.should eq(200)
+      events = JSON.parse(response.body).as_a
+      events.size.should eq(1)
+      events.map(&.["id"]).should_not contain(private_event.id)
+      events.map(&.["id"]).should contain(open_event.id)
+      events.map(&.["id"]).should contain(public_event.id)
     end
 
     pending "#index should return a list of PRIVATE, OPEN, and PUBLIC group-event events for the event creator" do
