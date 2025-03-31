5e4a09b2fcb5adce3c39d32fcbc4d7b0a1263021
Ali Naqvi <syed.alinaqvi@gmail.com>
2025-03-31 16:22:21 +0800

    feat: PPT-1994 added MS teams controller

---
+++ spec/controllers/teams_spec.cr
@@ -0,0 +1,41 @@
+require "../spec_helper"
+
+describe Teams do
+  client = AC::SpecHelper.client
+  headers = Mock::Headers.office365_guest
+
+  describe "#index" do
+    it "should return the list of messages (without the replies) in a channel of a team" do
+      WebMock.stub(:post, "https://login.microsoftonline.com/bb89674a-238b-4b7d-91ec-6bebad83553a/oauth2/v2.0/token")
+        .to_return(body: File.read("./spec/fixtures/tokens/o365_token.json"))
+      WebMock.stub(:get, "https://graph.microsoft.com/v1.0/teams/my_teams/channels/my_channel/messages")
+        .to_return(body: File.read("./spec/fixtures/teams/list.json"))
+
+      body = JSON.parse(client.get("#{TEAMS_BASE}/my_teams/my_channel", headers: headers).body).as_h
+      body["value"].as_a.size.should eq(3)
+    end
+
+    it "should return a single message or a message reply in a channel or a chat" do
+      WebMock.stub(:post, "https://login.microsoftonline.com/bb89674a-238b-4b7d-91ec-6bebad83553a/oauth2/v2.0/token")
+        .to_return(body: File.read("./spec/fixtures/tokens/o365_token.json"))
+      WebMock.stub(:get, "https://graph.microsoft.com/v1.0/teams/my_teams/channels/my_channel/messages/message_1")
+        .to_return(body: File.read("./spec/fixtures/teams/index_filtered.json"))
+
+      body = JSON.parse(client.get("#{TEAMS_BASE}/my_teams/my_channel/message_1", headers: headers).body).as_h
+      body["messageType"].as_s.should eq("message")
+    end
+  end
+
+  it "should send a new chatMessage in the specified channel or a chat" do
+    WebMock.stub(:post, "https://login.microsoftonline.com/bb89674a-238b-4b7d-91ec-6bebad83553a/oauth2/v2.0/token")
+      .to_return(body: File.read("./spec/fixtures/tokens/o365_token.json"))
+    WebMock.stub(:post, "https://graph.microsoft.com/v1.0/teams/my_teams/channels/my_channel/messages")
+      .to_return(status: 201)
+    headers["Content-Type"] = "text/plain"
+    body = client.post("#{TEAMS_BASE}/my_teams/my_channel", headers: headers, body: "Hello Teams")
+    pp body
+    body.status_code.should eq(201)
+  end
+end
+
+TEAMS_BASE = Teams.base_route
